a9889965075d5b8d33ce378c97c1f0ac37e0c0b0
Pzpito <del.kylian26@gmail.com>
2026-02-01 19:17:05 +0100

    fix menu

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,18 +1,13 @@
-// loader.js
 document.addEventListener("DOMContentLoaded", function() {
-
-    // 1. On cherche l'endroit où afficher le menu
     const menuContainer = document.getElementById("menu-placeholder");
 
     if (menuContainer) {
-        // 2. On va chercher le fichier
         fetch("/layout/nav.html")
             .then(response => {
                 if (!response.ok) throw new Error("Erreur chargement menu");
                 return response.text();
             })
             .then(html => {
-                // 3. On l'injecte dans la page
                 menuContainer.innerHTML = html;
             })
             .catch(error => console.error("Problème :", error));
